39719abd8a823b65d4046255056867012a4a22a2
IDEAPAD320S\LenovoDEV <pasakorn.s@outlook.com>
2018-07-13 14:13:16 +0700

    add code for Lab01

--- EmguCV_Labs/Lab01.Designer.cs
+++ EmguCV_Labs/Lab01.Designer.cs
@@ -28,20 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pboPictureBox = new Emgu.CV.UI.PanAndZoomPictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iboImageBox = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iboImageBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pboPictureBox
+            // 
+            this.pboPictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pboPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pboPictureBox.Location = new System.Drawing.Point(321, 46);
+            this.pboPictureBox.Name = "pboPictureBox";
+            this.pboPictureBox.Size = new System.Drawing.Size(294, 220);
+            this.pboPictureBox.TabIndex = 1;
+            this.pboPictureBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(438, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Load Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ImageBox";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(424, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PictureBox";
+            // 
+            // iboImageBox
+            // 
+            this.iboImageBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.iboImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.iboImageBox.Location = new System.Drawing.Point(12, 46);
+            this.iboImageBox.Name = "iboImageBox";
+            this.iboImageBox.Size = new System.Drawing.Size(294, 220);
+            this.iboImageBox.TabIndex = 2;
+            this.iboImageBox.TabStop = false;
             // 
             // Lab01
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 365);
+            this.ClientSize = new System.Drawing.Size(631, 324);
+            this.Controls.Add(this.iboImageBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pboPictureBox);
             this.Name = "Lab01";
             this.Text = "Lab 01 : Open Image";
             this.Load += new System.EventHandler(this.Lab01_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pboPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iboImageBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private Emgu.CV.UI.PanAndZoomPictureBox pboPictureBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Emgu.CV.UI.ImageBox iboImageBox;
     }
 }
